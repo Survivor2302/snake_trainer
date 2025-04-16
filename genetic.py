8851a8c125a3f0573defefd35d8b152cf435bbae
Léo Bournizien <73833842+Survivor2302@users.noreply.github.com>
2025-04-16 15:01:13 +0200

    Initial commit

---
+++ genetic.py
@@ -0,0 +1,47 @@
+import numpy
+from NeuralNetwork import *
+from snake import *
+
+def eval(sol, gameParams):
+    return 0
+
+'''
+Représente une solution avec
+_un réseau de neurones
+_un score (à maximiser)
+
+vous pouvez ajouter des attributs ou méthodes si besoin
+'''
+class Individu:
+    def __init__(self, nn):
+        self.nn = nn
+        self.score = 0
+
+
+'''
+La méthode d'initialisation de la population est donnée :
+_on génère N individus contenant chacun un réseau de neurones (de même format)
+_on évalue et on trie des individus
+'''
+def initialization(taillePopulation, arch, gameParams):
+    population = []
+    for i in range(taillePopulation):
+        nn = NeuralNetwork((arch[0],))
+        for j in range(1, len(arch)):
+            nn.addLayer(arch[j], "elu")
+        population.append(Individu(nn))
+
+    for sol in population: eval(sol, gameParams)
+    population.sort(reverse=True, key=lambda sol:sol.score)
+    
+    return population
+
+def optimize(taillePopulation, tailleSelection, pc, arch, gameParams, nbIterations, nbThreads, scoreMax):
+    population = initialization(taillePopulation, arch, gameParams)
+
+    '''
+    TODO : corps de l'algorithme principal
+    '''
+
+
+    return population[0].nn
---
+++ snake.py
@@ -0,0 +1,72 @@
+import random
+import itertools
+import numpy
+from NeuralNetwork import *
+
+nbFeatures = 8
+nbActions = 4
+
+class Game:
+    def __init__(self, hauteur, largeur):
+        self.grille = [[0]*hauteur  for _ in range(largeur)]
+        self.hauteur, self.largeur = hauteur, largeur
+        self.serpent = [[largeur//2-i-1, hauteur//2] for i in range(4)]
+        for (x,y) in self.serpent: self.grille[x][y] = 1
+        self.direction = 3
+        self.accessibles = [[x,y] for (x,y) in list(itertools.product(range(largeur), range(hauteur))) if [x,y] not in self.serpent]
+        self.fruit = [0,0]
+        self.setFruit()
+        self.enCours = True
+        self.steps = 0
+        self.score = 4
+    
+    def setFruit(self):
+        if (len(self.accessibles)==0): return
+        self.fruit = self.accessibles[random.randint(0, len(self.accessibles)-1)][:]
+        self.grille[self.fruit[0]][self.fruit[1]] = 2
+
+    def refresh(self):
+        nextStep = self.serpent[0][:]
+        match self.direction:
+            case 0: nextStep[1]-=1
+            case 1: nextStep[1]+=1
+            case 2: nextStep[0]-=1
+            case 3: nextStep[0]+=1
+
+        if nextStep not in self.accessibles:
+            self.enCours = False
+            return
+        self.accessibles.remove(nextStep)
+        if self.grille[nextStep[0]][nextStep[1]]==2:
+            self.setFruit()
+            self.steps = 0
+            self.score+=1
+        else:
+            self.steps+=1
+            self.grille[self.serpent[-1][0]][self.serpent[-1][1]] = 0
+            self.accessibles.append(self.serpent[-1][:])
+            self.serpent = self.serpent[:-1]
+            if self.steps>self.hauteur*self.largeur:
+                self.enCours = False
+                return
+
+        self.grille[nextStep[0]][nextStep[1]] = 1
+        self.serpent = [nextStep]+self.serpent
+
+    def getFeatures(self):
+        features = numpy.zeros(8)
+        
+        return features
+    
+    def print(self):
+        print("".join(["="]*(self.largeur+2)))
+        for ligne in range(self.hauteur):
+            chaine = ["="]
+            for colonne in range(self.largeur):
+                if self.grille[colonne][ligne]==1: chaine.append("#")
+                elif self.grille[colonne][ligne]==2: chaine.append("F")
+                else: chaine.append(" ")
+            chaine.append("=")
+            print("".join(chaine))
+        print("".join(["="]*(self.largeur+2))+"\n")
+
